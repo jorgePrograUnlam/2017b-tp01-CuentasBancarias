ef4281e91d1d7c8e3e1db449a2bc492cc818f3c5
jorge <jamc4626@gmail.com>
2017-09-09 01:30:23 -0300

    ROJO: se transfiere 100

--- cuentasBancarias/src/cuentasBancarias/TestCuentaBancaria.java
+++ cuentasBancarias/src/cuentasBancarias/TestCuentaBancaria.java
@@ -1,6 +1,6 @@
 package cuentasBancarias;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -14,7 +14,17 @@ public class TestCuentaBancaria {
 
 		cuentaOrigen.transferirMontoHacia(0, cuentaDestino);
 
-		assertEquals(cuentaOrigen.getSaldo(), cuentaDestino.getSaldo(),0.001);
+		assertEquals(cuentaOrigen.getSaldo(), cuentaDestino.getSaldo(), 0.001);
+	}
+
+	@Test
+	public void transferirCien() {
+		CuentaBancaria cuentaOrigen = new CuentaBancaria(100);
+		CuentaBancaria cuentaDestino = new CuentaBancaria(0);
+
+		cuentaOrigen.transferirMontoHacia(100, cuentaDestino);
+
+		assertEquals(100, cuentaDestino.getSaldo(), 0.001);
 	}
 
 }
