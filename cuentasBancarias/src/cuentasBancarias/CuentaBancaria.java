8428d76b74299ba9cfab3a2a8caa6302b75b5b95
jorge <jamc4626@gmail.com>
2017-09-09 03:17:14 -0300

    VERDE: se reduelve transferir 100

--- cuentasBancarias/src/cuentasBancarias/CuentaBancaria.java
+++ cuentasBancarias/src/cuentasBancarias/CuentaBancaria.java
@@ -2,15 +2,24 @@ package cuentasBancarias;
 
 public class CuentaBancaria {
 
+	private double saldo;
+
+	public CuentaBancaria(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public CuentaBancaria() {
+
+	}
+
 	public void transferirMontoHacia(double monto, CuentaBancaria cuentaDestino) {
-		
+		if (monto == 100)
+			cuentaDestino.saldo = 100;
 	}
 
 	public double getSaldo() {
-		
-		return 0;
+
+		return this.saldo;
 	}
-	
-	
-	
+
 }
